ee605241c880c2e8bd0c1528a4df3b7c5e9ce2d2
Steph St <steph.streete@outlook.com>
2021-01-16 22:20:59 +0000

    stuck on code

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+//ask the question and print
+//int main(){
+  //std::cout << "Is your string a Palindrome?\n";
 
-int main() {
-  std::cout << "Hello World!\n";
+//}
+int is_palindrome;
+//set functions
+int main(){
+  std::cout << "Type you sentence: \n";
+  //request input
+  std::cin >> is_palindrome;
+
+//set a function to see if string is reversed 
+  if (is_palindrome == is_palindrome (:: -1))
+  ;
+std::cout <<"Yes your sentence is a Palindrome!" "\n";
+  if (is_palindrome != is_palindrome)
+  ;
+std::cout <<"No your sentence is not a Palindrome!" "\n";
+
 }
+
