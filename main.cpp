4d08d2ad0a3dc74fa1c06d87b2c58afc7e00fbd6
Steph St <steph.streete@outlook.com>
2021-02-18 21:29:12 +0000

    changed functions and added more comments

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include <string>
+using namespace std;
+//ask the question and print
+int main(){
+  string input;
+  cout << "Type you sentence: ";
+  //request input
+  cin >> input;
 
-int main() {
-  std::cout << "Hello World!\n";
+//set a function to see if string is reversed 
+  if (input == string(input.rbegin(),input.rend())) 
+//print answers
+cout << input << "Yes your sentence is a Palindrome!" ;
+  else 
+  
+cout << input << "No your sentence is not a Palindrome!" ;
+return 0;
 }
+
